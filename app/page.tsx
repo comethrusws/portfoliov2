3f07189a508a23e0739e7b5caed9058f53654419
comethrusws <basabjha@naver.com>
2024-06-14 03:43:00 +0545

    Commit #3: Project section mockup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about";
 import Footer from "@/components/footer";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Intro/>
       <SectionDivider/>
       <About/>
+      <Projects/>
 
       <Footer/>
     </main>
