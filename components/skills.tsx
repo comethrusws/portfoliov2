49ce1a907dab984288c49cf6b33becc191ef06f3
comethrusws <basabjha@naver.com>
2024-06-14 13:54:53 +0545

    Commit #3.1: Skills tweaks(lightmode bg), Skill section header update

--- components/skills.tsx
+++ components/skills.tsx
@@ -33,7 +33,7 @@ export default function Skills() {
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
-            className="text-xs bg-white border-black/[0.1] rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+            className="text-xs cursor-pointer bg-gray-300 border-black rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
             key={index}
             variants={fadeInAnimationVariants}
             initial="initial"
