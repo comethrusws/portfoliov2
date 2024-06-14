46b44bef57cc9ca910ac4b7a1639b67848a9958b
comethrusws <basabjha@naver.com>
2024-06-14 15:52:23 +0545

    Commit #6.1: Color Changes for Light Mode + Email Toast Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='!scroll-smooth'>
       <body
-          className={`bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-800 dark:text-gray-50 dark:text-opacity-90`}
+          className={`bg-gray-100 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-500 dark:text-gray-50 dark:text-opacity-90`}
       >
         <ThemeContextProvider>
 
--- components/contact.tsx
+++ components/contact.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
     >
       <SectionHeading>Contact me</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
+      <p className="text-gray-300 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:basabjha.13@gmail.com">
           basabjha.13@gmail.com
