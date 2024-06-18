aa86fb36c09fed8f87cc185fc60af71d6010c9d8
comethrusws <basabjha@naver.com>
2024-06-18 11:10:46 +0545

    Commit #7.5: Updsted CV & Contact email

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
       <p className="text-gray-300 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:basabjha.13@gmail.com">
-          basabjha.13@gmail.com
+          basabjha.dev@gmail.com
         </a>{" "}
         or through this form.
       </p>
