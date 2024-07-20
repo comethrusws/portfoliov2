7c467dc8f67bbaa857c22a7d37d2fc4d04e493f5
comethrusws <basabjha@naver.com>
2024-07-20 18:16:17 +0545

    Commit #8.6: CV updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Resume - Basab Jha.pdf"
           download
         >
           Download CV{" "}
